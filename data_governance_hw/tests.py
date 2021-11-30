4aff5709240e990ef9a8885c35e5a055df393eec
Uladzimir Kapeika <volodyakopeyko@gmail.com>
2021-11-30 13:17:29 +0100

    Add unit tests

---
+++ data_governance_hw/tests.py
@@ -0,0 +1,104 @@
+import process_data
+import train
+import pandas as pd
+import string
+import nltk
+import time
+import pickle
+import numpy as np
+from sklearn.metrics import confusion_matrix
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+
+def test_input_data_shape():
+    df_train = pd.read_csv('train.csv', sep=",")
+    assert df_train.shape[0] > 0
+    assert df_train.shape[1] > 0
+    assert (0 < df_train['text'].apply(len)).all()
+    assert (280 > df_train['text'].apply(len)).all()
+    assert (df_train['target'].isin((0, 1))).all()
+    assert 0 < sum(df_train['target']) < df_train.shape[0]
+    
+    
+def test_cleaning_text_function():
+    test1 = """My cousin in Trinidad won’t get the vaccine cuz his friend got it & became impotent. 
+        his friend was weeks away from getting married, now the girl called off the wedding. 
+        So just pray on it & make sure you’re comfortable with ur decision, not bullied"""
+    processed_text = process_data.tokenize_sentence(test1)
+    print(processed_text)
+    print(stopwords.words("english"))
+    assert len(set(processed_text.split()) & set(string.punctuation)) == 0
+    #assert len(set(processed_text.split()) & set(stopwords.words("english"))) == 0
+    
+    
+def test_processed_data_for_training_purposes():
+    df_train = pd.read_csv('train.csv', sep=",")
+    df_train['text'] = df_train['text'].apply(process_data.tokenize_sentence)
+    df_train.to_csv('data_processed_train.csv')
+    assert all(df_train['text'].apply(len) > 0)
+    
+
+def test_text_vectorizer():
+    df_processed = pd.read_csv('data_processed_train.csv', sep=",")
+    vectorizer, features = train.prepare_data(df_processed)
+    features = features.toarray()
+    assert features.shape[0] > 0
+    assert (features <= 1).all()
+    filename = 'vectorizer.sav'
+    pickle.dump(vectorizer, open(filename, 'wb'))
+
+
+def test_training_time():
+    df_processed = pd.read_csv('data_processed_train.csv', sep=",")
+    _, features = train.prepare_data(df_processed)
+    X_train, y_train = features, df_processed['target']
+    time_before = time.time()
+    model, y_pred = train.train_model(X_train, y_train)
+    assert time.time() - time_before < 600
+    filename = 'model.sav'
+    pickle.dump(model, open(filename, 'wb'))
+
+
+def test_inference_time():
+    test_text = 'this is a test tweet to check the inference time'
+    processed_text = process_data.tokenize_sentence(test_text)
+    print(processed_text)
+    filename = 'model.sav'
+    model = pickle.load(open(filename, 'rb'))
+
+    filename = 'vectorizer.sav'
+    vectorizer = pickle.load(open(filename, 'rb'))
+
+    df_temp = pd.DataFrame()
+    df_temp['text'] = [processed_text]
+    print(processed_text)
+    print(df_temp)
+    features = vectorizer.transform(df_temp['text'])
+    time_before = time.time()
+    result = model.predict(features)
+    assert time.time() - time_before < 100
+
+
+def test_metrics_values():
+    df_processed = pd.read_csv('data_processed_train.csv', sep=",")
+    _, features = train.prepare_data(df_processed)
+    X_train, y_train = features, df_processed['target']
+    model, y_pred = train.train_model(X_train, y_train)
+    acc, specificity, sensitivity = train.calculate_metrics(y_pred, y_train)
+    assert 0.5 < acc < 1
+    assert 0 < specificity < 1
+    assert 0 < sensitivity < 1
+
+
+def test_permutation_importance_results():
+    df_processed = pd.read_csv('data_processed_train.csv', sep=",")
+    _, features = train.prepare_data(df_processed)
+    X_train, y_train = features, df_processed['target']
+    filename = 'model.sav'
+    model = pickle.load(open(filename, 'rb'))
+    pi_results = train.calculate_permute_importance(model, X_train, y_train)
+    assert len(pi_results == 10)
+
+
--- data_governance_hw/train.py
+++ data_governance_hw/train.py
@@ -1,23 +1,17 @@
 import pandas as pd
 import numpy as np
 import json
-import seaborn as sns
-import matplotlib.pyplot as plt
 import sklearn.linear_model as lm
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_predict
 from sklearn.inspection import permutation_importance
 from matplotlib import pyplot
-from sklearn.metrics import roc_curve
-from sklearn.metrics import precision_score, recall_score, precision_recall_curve, f1_score
 
-df_train = pd.read_csv('data_processed_train.csv')
-df_test = pd.read_csv('data_processed_test.csv')
+
 
 vectorizer_params = {
-    "max_features": 1000,
+    "max_features": 50,
     #"max_features": None,
     #"ngram_range": (1, 2),
     #"min_df": 0,
@@ -28,7 +22,7 @@ vectorizer_params = {
     #"analyzer": "char"
 }
 lr_model_params = {
-    #"class_weight": "balanced",
+    "class_weight": "balanced",
     #"class_weight": None,
     #"class_weight": {1: 1, 0: 1/class_ratio},
     #"random_state": 0,
@@ -41,34 +35,49 @@ lr_model_params = {
     #"cv": 10
 }
 
-vectorizer = TfidfVectorizer(**vectorizer_params)
-features = vectorizer.fit_transform(df_train["text"])
 
-X_train = features
+def prepare_data(df_train):
+    vectorizer = TfidfVectorizer(**vectorizer_params)
+    features = vectorizer.fit_transform(df_train["text"])
+    return vectorizer, features
+
+
+def train_model(X_train, y_train):
+    model = lm.LogisticRegressionCV(**lr_model_params)
+    y_pred = cross_val_predict(model, X_train, y_train, cv=5)
+    model.fit(X_train, y_train)
+    return model, y_pred
+
+
+def calculate_metrics(y_pred, y_train):
+    acc = np.mean(y_pred == y_train)
+    tn, fp, fn, tp = confusion_matrix(y_train, y_pred).ravel()
+    specificity = tn / (tn+fp)
+    sensitivity = tp / (tp + fn)
+    return acc, specificity, sensitivity
+
+
+def calculate_permute_importance(model, X_train, y_train):
+    model.fit(X_train, y_train)
+    results = permutation_importance(model, X_train.toarray(), y_train, scoring='accuracy')
+    importances = results.importances_mean
+
+    return importances
+
+
+df_train = pd.read_csv('data_processed_train.csv')
+_, X_train = prepare_data(df_train)
 y_train = df_train['target']
 
-model = lm.LogisticRegressionCV(**lr_model_params)
-y_pred = cross_val_predict(model, X_train, y_train, cv=5)
+model, y_pred = train_model(X_train, y_train)
 
-#test_features = vectorizer.transform(df_test["text"])
-#y_test = df_test['target']
-#y_pred = model.predict(test_features)
-
-acc = np.mean(y_pred == y_train)
-tn, fp, fn, tp = confusion_matrix(y_train, y_pred).ravel()
-specificity = tn / (tn+fp)
-sensitivity = tp / (tp + fn)
-
+acc, specificity, sensitivity = calculate_metrics(y_pred, y_train)
 with open("metrics.json", 'w') as outfile:
     json.dump({"accuracy": acc, "specificity": specificity, "sensitivity": sensitivity}, outfile)
+importance = calculate_permute_importance(model, X_train, y_train)
 
-model.fit(X_train, y_train)
-print(X_train.toarray().shape)
-results = permutation_importance(model, X_train.toarray(), y_train, scoring='accuracy')
-importance = results.importances_mean
-
-for i,v in enumerate(importance[0:10]):
+for i, v in enumerate(importance[0:10]):
 	print('Feature: %0d, Score: %.5f' % (i,v))
 
-ax = pyplot.bar([x for x in range(len(importance[0:10]))], importance)
+ax = pyplot.bar([x for x in range(len(importance[0:10]))], importance[0:10])
 pyplot.savefig("permutate_feature_importance_top_10.png", dpi=80)
