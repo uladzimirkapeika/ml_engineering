82244435b3201fc1f6e6e9e7d79b7606e65e36c1
Uladzimir Kapeika <volodyakopeyko@gmail.com>
2021-12-02 13:35:28 +0100

    Change model from Logistic regression to RandomForest

--- data_governance_hw/train.py
+++ data_governance_hw/train.py
@@ -2,23 +2,19 @@ import json
 import pandas as pd
 import numpy as np
 import sklearn.linear_model as lm
+import seaborn as sns
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_predict
 from sklearn.inspection import permutation_importance
-from matplotlib import pyplot
+from sklearn.ensemble import RandomForestClassifier
+
 
 
 vectorizer_params = {
     "max_features": 220,
-    # "max_features": None,
     "ngram_range": (1, 2),
-    # "min_df": 0,
-    # "max_df": 100,
     "use_idf": True,
-    # "decode_error": "replace",
-    # "sublinear_tf": True,
-    # "analyzer": "char"
 }
 lr_model_params = {
     "class_weight": "balanced",
@@ -35,7 +31,7 @@ def prepare_data(df):
 
 def train_model(X, y):
     """Train model and calculate cross validation predictions"""
-    model = lm.LogisticRegressionCV(**lr_model_params)
+    model = RandomForestClassifier(max_depth=2, random_state=0)
     y_pred = cross_val_predict(model, X, y, cv=5)
     model.fit(X, y)
     return model, y_pred
@@ -60,7 +56,7 @@ def calculate_permute_importance(model, X, y):
 
 
 df_train = pd.read_csv("data_processed_train.csv")
-_, X_train = prepare_data(df_train)
+vectorizer, X_train = prepare_data(df_train)
 y_train = df_train["target"]
 
 final_model, y_predictions = train_model(X_train, y_train)
@@ -72,8 +68,12 @@ with open("metrics.json", "w") as outfile:
         outfile,
     )
 permutate_importance = calculate_permute_importance(final_model, X_train, y_train)
-
-ax = pyplot.bar(
-    list(range(len(permutate_importance[0:10]))), permutate_importance[0:10]
-)
-pyplot.savefig("permutate_feature_importance_top_10.png", dpi=80)
+ind = np.argpartition(permutate_importance, -10)[-10:]
+top_10_words = np.array(vectorizer.get_feature_names())[ind]
+sns.set(rc={'figure.figsize':(18, 12)})
+ax = sns.barplot(x=top_10_words, y=permutate_importance[ind],
+            label='Total', color='b', edgecolor='w')
+ax.tick_params(axis='x', rotation=45)
+ax.set(xlabel='Word', ylabel='Permutate importance')
+sns.set_color_codes('muted')
+ax.figure.savefig("permutate_feature_importance_top_10.png", dpi=80)
